333301fa86d41f73c11a40a067e7f79062cb17db
SandyTwix <iamsandytwix@gmail.com>
2023-01-21 20:13:45 +0300

    Initial Commit

---
+++ Productivity/ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityApp.swift
+//  Productivity
+//
+//  Created by Руслан Трищенков on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
